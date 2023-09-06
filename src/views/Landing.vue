ce0bee5f9067f05df6e235a3326eb5917fb84e24
Akinpelumi Taiwo <68600360+Twain97@users.noreply.github.com>
2023-09-06 12:38:10 -1100

    border to register on medium size

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -20,13 +20,13 @@
         <div class="h-full  md:w-2/4 ">
                                 <!-- Log in -->
 
-          <div v-if="showLogin" class="z-50 flex py-6 md:flex w-full h-full md:p-0">
+          <div v-if="showLogin" class="z-50 flex py-6 md:flex w-full md:border-4 md:border-red-600 h-full md:p-0">
           <div class="w-full h-4/5 mt-24 flex flex-col px-2 md:w-full md:h-4/5 m-auto md:mt-auto  md:justify-between">
             <img :src="logo" alt="logo" class="w-16 -mt-24 md:w-28 md:m-auto  mx-auto">
           
             <carousel class="md:hidden shadow-md rounded-lg mx-auto h-5/6 overflow-hidden " :items-to-show="0.7" :autoplay="2500" :wrap-around="true" :show-arrows="false">
               <slide class=" w-fit " v-for="slide in images" :key="slide">
-                <img class="w-full h-full ml-5 -mt-12"  
+                <img class="w-full h-full ml-5 -mt-16"  
                 :src="slide" alt="">
               </slide>
               
